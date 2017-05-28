f8828b41b0b7b1aa6f01496b7f54d6047aa0589e
AceAustin17 <a.naidoo17@gmail.com>
2017-05-29 00:02:05 +0200

    Created prototype for D5

--- Compression Year Project/Compression Year Project/MainWindow.xaml.cs
+++ Compression Year Project/Compression Year Project/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace Compression_Year_Project
 {
@@ -24,5 +26,65 @@ namespace Compression_Year_Project
         {
             InitializeComponent();
         }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "All Usable Files (*.mp3;*.jpg;*txt;*.mp4)|*.mp3;*.jpg;*.txt;*.mp4|Audio Files (*.mp3)|*.mp3|Image Files (*.jpg)|*.jpg|Text Files (*.txt)|*.txt|Video Files (*.mp4)|*.mp4";
+            if(opf.ShowDialog() == true)
+            {
+                txtMain.Text += "The file loaded is "+System.IO.Path.GetFileName(opf.FileName) + "\n";
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Canvas_PreviewDrop(object sender, DragEventArgs e)
+        {
+            txtMain.Text += "The file loaded is "+ System.IO.Path.GetFileName((string)((DataObject)e.Data).GetFileDropList()[0]) + "\n";
+        }
+
+        private void Canvas_PreviewDragOver(object sender, DragEventArgs e)
+        {
+            bool check = false;
+            FileInfo f = new FileInfo((string)((DataObject)e.Data).GetFileDropList()[0]);
+            
+            switch (f.Extension)
+            {
+                case ".txt":
+                    check = true;
+                    break;
+                case ".mp4":
+                    check = true;
+                    break;
+                case ".mp3":
+                    check = true;
+                    break;
+                case ".jpg":
+                    check = true;
+                    break;
+                case ".JPG":
+                    check = true;
+                    break;
+                default:
+                    check = false;
+                 break;
+            }
+
+            if(!check)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.All;
+            }
+            e.Handled = true;
+
+        }
+
     }
 }
