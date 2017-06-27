5a19f9475a54ae3b25b7a41cdf24be34dd1335f9
AceAustin17 <a.naidoo17@gmail.com>
2017-06-27 12:26:51 +0200

    Created support classes for network trainer class

---
+++ Compression Year Project/ANeuralNetwork/NetworkTrainer.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ANeuralNetwork
+{
+    public class DataPoint
+    {
+        public DataPoint() { }
+        public DataPoint(double[] input, double[] output) { Load(input, output); }
+
+        public DataPoint(XmlElement xelem) { Load(xelem); }
+
+        public double[] input;
+        public double[] output;
+
+        public void Load(double[] input, double[] output)
+        {
+            this.input = new double[input.Length];
+            this.output = new double[output.Length];
+            Array.Copy(input, this.input, input.Length);
+            Array.Copy(output, this.output, output.Length);
+        }
+
+        public void Load(XmlElement xElem)
+        {
+            XmlNode nType;
+            int lIn, lOut, i;
+
+            nType = xElem.SelectSingleNode("Input");
+
+            lIn = nType.ChildNodes.Count;
+
+            input = new double[lIn];
+
+            foreach (XmlNode node in nType.ChildNodes)
+            {
+                XmlElement enode = (XmlElement)node;
+                double val;
+
+                int.TryParse(enode.GetAttribute("Index"), out i);
+                double.TryParse(enode.InnerText, out val);
+
+                input[i] = val;
+            }
+
+            nType = xElem.SelectSingleNode("Output");
+
+            lOut = nType.ChildNodes.Count;
+
+            output = new double[lOut];
+
+            foreach (XmlNode node in nType.ChildNodes)
+            {
+                XmlElement enode = (XmlElement)node;
+                double val;
+
+                int.TryParse(enode.GetAttribute("Index"), out i);
+                double.TryParse(enode.InnerText, out val);
+
+                output[i] = val;
+            }
+        }
+
+        public XmlElement toXml(XmlDocument xdoc)
+        {
+            XmlElement nDatapoint, ntype, node;
+
+            int lIn = input.Length;
+            int lOut = output.Length;
+
+            nDatapoint = xdoc.CreateElement("Datapoint");
+            ntype = xdoc.CreateElement("Input");
+
+            for (int i = 0; i < lIn; i++)
+            {
+                node = xdoc.CreateElement("Data");
+                node.SetAttribute("Index", i.ToString());
+                node.AppendChild(xdoc.CreateTextNode(input[i].ToString()));
+                ntype.AppendChild(node);
+            }
+
+            nDatapoint.AppendChild(ntype);
+
+            ntype = xdoc.CreateElement("Output");
+
+            for (int i = 0; i < lOut; i++)
+            {
+                node = xdoc.CreateElement("Data");
+                node.SetAttribute("Index", i.ToString());
+                node.AppendChild(xdoc.CreateTextNode(output[i].ToString()));
+                ntype.AppendChild(node);
+            }
+
+            nDatapoint.AppendChild(ntype);
+
+            return nDatapoint;
+        }
+    }
+
+    public class DataSet
+    {
+        public List<DataPoint> data;
+        public DataSet() { data = new List<DataPoint>(); }
+
+        public XmlElement toXml(XmlDocument doc)
+        {
+            XmlElement nDataset;
+
+            nDataset = doc.CreateElement("DataSet");
+
+            foreach(DataPoint d in data)
+            {
+                nDataset.AppendChild(d.toXml(doc));
+
+            }
+
+            return nDataset;    
+        }
+
+        public void Load(XmlElement dataset)
+        {
+            foreach(XmlNode node in dataset.ChildNodes)
+            {
+                DataPoint dp = new DataPoint((XmlElement)node);
+                data.Add(dp);
+            }
+        }
+    }
+
+    public class Permutator
+    {
+        public Permutator(int size)
+        {
+            indices = new int[size];
+
+            for(int i = 0; i < size; i++)
+            {
+                indices[i] = i;
+            }
+            permute(size);
+        }
+        private int[] indices;
+        private Random ran = new Random();
+
+        private void permute(int times)
+        {
+            int i, j, t;
+            for(int n= 0;n < times;n++)
+            {
+                i = ran.Next(indices.Length);
+                j = ran.Next(indices.Length);
+
+                if(i != j)
+                {
+                    t = indices[i];
+                    indices[i] = indices[j];
+                    indices[j] = t;
+                }
+            }
+        }
+
+        public  int this[int i]
+        {
+            get
+            {
+                return indices[i];
+            }
+        }
+            }
+
+
+}
--- Compression Year Project/Compression Year Project/MainWindow.xaml.cs
+++ Compression Year Project/Compression Year Project/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 using ANeuralNetwork;
+using System.Xml;
 
 namespace Compression_Year_Project
 {
@@ -26,38 +27,7 @@ namespace Compression_Year_Project
         public MainWindow()
         {
             InitializeComponent();
-            int[] ls = new int[3] { 1, 2, 1 };
-            ActivationFunction[] af = new ActivationFunction[3] { ActivationFunction.None,
-                                                                ActivationFunction.RationalSig,
-                                                                ActivationFunction.Linear };
-            BackPropNetwork bp = new BackPropNetwork(ls,af);
-
-            double[] input = new double[1] { 1.0};
-            double[] desired = new double[1] { 2.5 };
-            double[] output = new double[1];
-
-            double error = 0.0;
-
-            for(int i = 0; i < 1000; i++)
-            {
-                error = bp.train(ref input, ref desired, 0.15, 0.1);
-                bp.run(ref input, out output);
-
-                if (i % 100 == 0)
-                {
-                 txtMain.Text += "Iteration " + i + ":\n\tInput " + Math.Round(input[0],3) + " Output " + Math.Round(output[0],3) + " Error " + Math.Round(error,3) + '\n';
-                }
-            }
-
-           bp.Save("../Ann.xml");
-
-            BackPropNetwork bp2 = new BackPropNetwork("../Ann.xml");
-
-            bp2.run(ref input, out output);
-
-            txtMain.Text += "Input " + Math.Round(input[0], 3) + " Output " + Math.Round(output[0], 3);
-
-
+           
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
