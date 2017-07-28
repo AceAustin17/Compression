a427e96c83cc9e4ee7a3830ec845d8ebc0b41ab6
AceAustin17 <a.naidoo17@gmail.com>
2017-07-28 19:46:49 +0200

    progress n extract txt

--- Compression Year Project/Compression Year Project/Extract.cs
+++ Compression Year Project/Compression Year Project/Extract.cs
@@ -10,6 +10,7 @@ namespace Compression_Year_Project
     class Extract
     {
         private string fileContents;
+        private List<KeyValuePair<string, string>> indexList = new List<KeyValuePair<string, string>>();
 
         public Extract(byte[] firstinput)
         {
@@ -21,12 +22,32 @@ namespace Compression_Year_Project
             string pattern = @"(\[\w*, \w*])|(\[\w*\n\w*, \w*])";
 
            Match m = Regex.Match(fileContents, pattern);
-            string t = "";
+           string index = "";
            while(m.Success)
             {
                 Group g = m.Groups[0];
-                t += g.ToString();
+                index += g.ToString();
                 m = m.NextMatch();
+            }
+
+            char[] separatingChars = {']'};      
+
+           
+            index = index.Trim();
+            string[] pairs = index.Split(separatingChars,System.StringSplitOptions.RemoveEmptyEntries);
+
+            string t = "";
+            for(int i =0; i < pairs.Length; i++)
+            {
+               pairs[i] = pairs[i].Trim('[',']',' ');
+               string[] chk = pairs[i].Split(',');
+
+               indexList.Add(new KeyValuePair<string, string>(chk[0], chk[1]));
+               
+            }
+            foreach(KeyValuePair<string,string> kv in indexList)
+            {
+                t += kv.Key + " " + kv.Value + "\n\n";
             }
             return t;
         }
